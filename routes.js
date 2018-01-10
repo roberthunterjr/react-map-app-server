09973eac2698b7d9f3689ada847f4a228707aee6
Robert Hunter Jr <robert_hunterjr@utexas.edu>
2018-01-10 17:16:46 -0600

    Added CORS for remote client
    Also modified the test routes to allow for passing addresses as object

--- routes.js
+++ routes.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
 const helper = require('./helpers.js');
 
+router.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 router.get('/hello', (req, res) => {
   res.send('Hello World');
 });
@@ -20,14 +26,26 @@ router.get('/testgeo', (req, res) => {
 });
 
 router.get('/testget', (req, res) => {
-  const bundle = decodeURI(JSON.parse(req.query.bundle));
+  const testData = {
+    one: '6408 Convict Hill Rd, Austin Texas, 78749',
+    two: '2901 S Capital of Texas Hwy, Austin, TX 78746'
+  }
+  const reqBundle = encodeURI(JSON.stringify(testData))
+  const bundle = JSON.parse(decodeURI(reqBundle));
+  console.log(bundle)
   res.status(200).send(bundle);
 })
 
 router.get('/getPlaces', (req, res) => {
-
-  console.log(req.query.address);
-  res.send(req.query.address);
+  const bundle = JSON.parse(decodeURI(req.query.bundle));
+  helpers.getPlacesInRange(bundle.one, bundle.two)
+  .then((result) => {
+    res.send(result);
+  })
+  .catch((err) => {
+    console.log('getGeo failure');
+    res.status(404).send('There was an error');
+  })
 })
 
 module.exports = router;
